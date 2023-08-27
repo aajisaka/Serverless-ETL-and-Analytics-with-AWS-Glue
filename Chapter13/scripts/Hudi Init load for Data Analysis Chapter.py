984920ad9e1ba0efe033165249d65885778f8cea
Akira Ajisaka <aajisaka@apache.org>
2023-08-27 15:32:49 +0900

    Update 02 job script

--- Chapter13/scripts/Hudi Init load for Data Analysis Chapter.py
+++ Chapter13/scripts/Hudi Init load for Data Analysis Chapter.py
@@ -3,8 +3,7 @@ from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
-from pyspark.sql.functions import col, to_timestamp, monotonically_increasing_id, to_date, when, lit
+from pyspark.sql.functions import lit
 from awsglue.utils import getResolvedOptions
 from pyspark.sql.types import *
 from datetime import datetime
@@ -21,24 +20,21 @@ inputDf = glueContext.create_data_frame.from_catalog(
     database='chapter_data_analysis_glue_database',
     table_name='employees').withColumn('ts', lit(datetime.now()))
 
-commonConfig = {'hoodie.datasource.hive_sync.use_jdbc': 'false',
-                'hoodie.datasource.write.recordkey.field': 'emp_no',
-                'hoodie.table.name': 'employees_cow',
-                'hoodie.consistency.check.enabled': 'true',
-                'hoodie.datasource.hive_sync.database': 'chapter_data_analysis_glue_database',
-                'hoodie.datasource.hive_sync.table': 'employees_cow',
-                'hoodie.datasource.hive_sync.enable': 'true',
-                'path': 's3://'+args['TARGET_BUCKET']+'/hudi/employees_cow_data'}
-
-unpartitionDataConfig = {'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.NonPartitionedExtractor',
-                         'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.NonpartitionedKeyGenerator'}
-
-initLoadConfig = {'hoodie.bulkinsert.shuffle.parallelism': 3,
-                  'hoodie.datasource.write.operation': 'bulk_insert'}
-
-combinedConf = {**commonConfig, **unpartitionDataConfig, **initLoadConfig}
+config = {
+    'hoodie.datasource.hive_sync.use_jdbc': 'false',
+    'hoodie.datasource.write.recordkey.field': 'emp_no',
+    'hoodie.table.name': 'employees_cow',
+    'hoodie.consistency.check.enabled': 'true',
+    'hoodie.datasource.hive_sync.database': 'chapter_data_analysis_glue_database',
+    'hoodie.datasource.hive_sync.table': 'employees_cow',
+    'hoodie.datasource.hive_sync.enable': 'true',
+    'path': 's3://'+args['TARGET_BUCKET']+'/hudi/employees_cow_data',
+    'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.NonPartitionedExtractor',
+    'hoodie.datasource.write.keygenerator.class': 'org.apache.hudi.keygen.NonpartitionedKeyGenerator',
+    'hoodie.bulkinsert.shuffle.parallelism': 3,
+    'hoodie.datasource.write.operation': 'bulk_insert'}
 
 inputDf.write.format('hudi') \
-  .options(combinedConf) \
+  .options(**config) \
   .mode('overwrite') \
   .save()
